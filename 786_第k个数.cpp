7b4d6bd920a08624d7caa3db9cea57a86fcd4ae9
DavidLee528 <35065046+DavidLee528@users.noreply.github.com>
2021-04-20 15:58:22 +0800

    Add files via upload

---
+++ 786_第k个数.cpp
@@ -0,0 +1,47 @@
+/*
+ * @ Author: 李天昊
+ * @ Description: 
+ * @ Date: 20210420
+ * @ E-mail: 13121515269@163.com
+ */
+
+#include <iostream>
+
+using namespace std; 
+
+const int N = 1e6 + 10; 
+
+int n; 
+int k; 
+int arr[N]; 
+
+void quick_sort(int *arr, int lhs, int rhs) {
+
+    if (lhs >= rhs) return ; 
+
+    int x = arr[ (lhs + rhs) >> 1 ]; 
+    int i = lhs - 1; 
+    int j = rhs + 1; 
+
+    while (i < j) {
+        do i++; while (arr[i] < x); 
+        do j--; while (arr[j] > x); 
+        if (i < j) swap(arr[i], arr[j]); 
+    }
+
+    quick_sort(arr, lhs, j); 
+    quick_sort(arr, j + 1, rhs); 
+
+}
+
+int main() {
+
+    scanf("%d%d", &n, &k); 
+    for (int i = 0; i < n; ++i) scanf("%d", &arr[i]); 
+
+    quick_sort(arr, 0, n - 1); 
+    printf("%d\n", arr[k - 1]); 
+    
+
+    return 0; 
+}
